b36ade63d6de1e08fad301e050bb3ad0785c2029
hasiburrahmanbiplab <biplobjr29@gmail.com>
2024-01-29 11:10:10 +0600

    commit-1

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$('.counter').counterUp({
+    delay: 10,
+    time: 1000
+});
+
+ function show(){
+     document.querySelector('.menu-btn').classList.toggle('open')
+     document.querySelector('.navigation').classList.toggle('active')
+}
+
+
+
